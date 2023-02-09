7e31beb6954aa1354ec30e3e172f3f2611bd6194
RenauxLea <lea.renaux@mail-ecv.fr>
2023-02-09 13:59:44 +0100

    add date picker and input

--- client/components/CreateIngredient.tsx
+++ client/components/CreateIngredient.tsx
@@ -1,20 +1,77 @@
 import React from "react";
 import {
+    Button,
     SafeAreaView,
     ScrollView,
     StyleSheet,
     Text,
+    TextInput,
     View,
+    
   } from 'react-native';
+import DatePicker from "react-native-date-picker";
 
+  
 export const CreateIngredient = () => { 
+    const [name, setName] = React.useState('');
+    const [quantity, setQuantity] = React.useState('');
+    const [date, setDate] = React.useState(new Date());
+    const [visibleDate, setVisibleDate] = React.useState(false); 
+
+    let expirationDate =""
+    if (date !== undefined) {
+        expirationDate =date.getDate() +'/'+(date.getMonth()+1)+'/'+date.getFullYear();
+    }
+   
+    
+    const [open, setOpen] = React.useState(false);
+
+    
    return (
     <SafeAreaView>
         <ScrollView  
         contentInsetAdjustmentBehavior="automatic"
         > 
             <View >
-                <Text style={styles.title}>page de création d'un ingrédient</Text>
+                <Text style={styles.title}>Nom:</Text>
+                <TextInput
+                    placeholder="navet"
+                    onChangeText={text => setName(text)}
+                    defaultValue={name}
+                />
+                <Text style={styles.title}>Catégorie:</Text>
+               
+                
+
+                <Text style={styles.title}>Quantité:</Text>
+                <TextInput
+                    keyboardType='numeric'
+                    onChangeText={number => setQuantity(number)}
+                    value={quantity}
+                    placeholder='10'
+                    maxLength={10}
+                />
+                <Text style={styles.title}>Unité:</Text>
+                <Text style={styles.title}>Date de péremption:</Text>
+                <Text >
+                    {visibleDate && expirationDate } 
+                </Text> 
+
+                <Button title="Open" onPress={() => setOpen(true)} />
+                <DatePicker
+                    modal
+                    open={open}
+                    date={date}
+                    onConfirm={(date) => {
+                    setOpen(false)
+                    setDate(date)
+                    setVisibleDate(true)
+                    }}
+                    onCancel={() => {
+                    setOpen(false)
+                    }}
+                />
+
                 
             </View>
         </ScrollView>
