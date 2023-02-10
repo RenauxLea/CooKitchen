4d1349cc9623e52e1ff4ff50dbbd9597104ec3b5
RenauxLea <lea.renaux@mail-ecv.fr>
2023-02-10 07:47:06 +0100

    add select

--- client/components/CreateIngredient.tsx
+++ client/components/CreateIngredient.tsx
@@ -10,6 +10,7 @@ import {
     
   } from 'react-native';
 import DatePicker from "react-native-date-picker";
+import SelectDropdown from "react-native-select-dropdown";
 
   
 export const CreateIngredient = () => { 
@@ -17,16 +18,28 @@ export const CreateIngredient = () => {
     const [quantity, setQuantity] = React.useState('');
     const [date, setDate] = React.useState(new Date());
     const [visibleDate, setVisibleDate] = React.useState(false); 
-
+    const [open, setOpen] = React.useState(false);
+    const [category , setCategory] = React.useState("");
+    const [unit, setUnit] = React.useState("");
+ 
     let expirationDate =""
     if (date !== undefined) {
         expirationDate =date.getDate() +'/'+(date.getMonth()+1)+'/'+date.getFullYear();
     }
    
-    
-    const [open, setOpen] = React.useState(false);
+    const categories = [ 
+        {name : "légume", id: "vegetable"} ,
+        {name: "fruit", id:"fruit"},
+        {name: "poisson", id:"fish"},
+        {name: "viande", id: "meat"},
+        {name: "céréales", id: "cereal"},
+        {name: "produit laitier", id: "milkProduct"},
+        {name: "aliment sucré", id: "sweetProduct"},
+        {name: "autre", id: "other"},
+    ]
 
-    
+    const units = [ "g", "cl", "aucune"];
+       
    return (
     <SafeAreaView>
         <ScrollView  
@@ -39,9 +52,22 @@ export const CreateIngredient = () => {
                     onChangeText={text => setName(text)}
                     defaultValue={name}
                 />
+
                 <Text style={styles.title}>Catégorie:</Text>
-               
-                
+                <SelectDropdown
+                    data={categories}
+
+                    onSelect={(selectedItem) => {
+                        setCategory(selectedItem.id)
+                    }}
+                    onChangeSearchInputText={() => {}}
+                    buttonTextAfterSelection={(selectedItem, index) => {
+                        return selectedItem.name
+                    }}
+                    rowTextForSelection={(item) => {
+                        return item.name
+                    }}
+                /> 
 
                 <Text style={styles.title}>Quantité:</Text>
                 <TextInput
@@ -52,6 +78,23 @@ export const CreateIngredient = () => {
                     maxLength={10}
                 />
                 <Text style={styles.title}>Unité:</Text>
+                <SelectDropdown
+                    data={units}
+
+                    onSelect={(selectedItem) => {
+                        setUnit(selectedItem)
+                    }}
+                    onChangeSearchInputText={() => {}}
+                    buttonTextAfterSelection={(selectedItem, index) => {
+                        return selectedItem
+                    }}
+                    rowTextForSelection={(item) => {
+                        return item
+                    }}
+                /> 
+
+
+
                 <Text style={styles.title}>Date de péremption:</Text>
                 <Text >
                     {visibleDate && expirationDate } 
